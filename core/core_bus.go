c9bcdc3fc4667fa665009a0c45c919168e07845f
Christian Stewart <christian@paral.in>
2019-02-02 04:47:03 -0800

    feat: load bifrost core factories
    Signed-off-by: Christian Stewart <christian@paral.in>

--- core/core_bus.go
+++ core/core_bus.go
@@ -3,6 +3,7 @@ package core
 import (
 	"context"
 
+	bifrostcore "github.com/aperturerobotics/bifrost/core"
 	nctr "github.com/aperturerobotics/bifrost/peer/controller"
 	"github.com/aperturerobotics/controllerbus/bus"
 	"github.com/aperturerobotics/controllerbus/controller"
@@ -33,6 +34,7 @@ func NewCoreBus(
 	sr.AddFactory(lookup_concurrent.NewFactory(b))
 	sr.AddFactory(volume_kvtxinmem.NewFactory(b))
 	sr.AddFactory(hydraeg.NewFactory(b))
+	bifrostcore.AddFactories(b, sr)
 
 	addNativeFactories(b, sr)
 	return b, sr, nil
