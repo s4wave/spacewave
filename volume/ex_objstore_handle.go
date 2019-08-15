24d03edc27d82fac7bd2669a8d0f510ae3afdc72
Christian Stewart <christian@paral.in>
2019-08-15 00:33:12 -0700

    feat: add build objstore api func to volume
    Signed-off-by: Christian Stewart <christian@paral.in>

---
+++ volume/ex_objstore_handle.go
@@ -0,0 +1,38 @@
+package volume
+
+import (
+	"context"
+	"errors"
+	"github.com/aperturerobotics/controllerbus/bus"
+	"github.com/aperturerobotics/controllerbus/directive"
+)
+
+// BuildObjectStoreAPIEx executes building an object store api.
+func BuildObjectStoreAPIEx(
+	ctx context.Context,
+	b bus.Bus,
+	storeID, storeVolume string,
+) (BuildObjectStoreAPIValue, directive.Reference, error) {
+	// Acquire handle to storage.
+	osv, osRef, err := bus.ExecOneOff(
+		ctx,
+		b,
+		NewBuildObjectStoreAPI(
+			storeID, storeVolume,
+		),
+		nil,
+	)
+	if err != nil {
+		return nil, nil, err
+	}
+	objs, ok := osv.GetValue().(BuildObjectStoreAPIValue)
+	if !ok {
+		osRef.Release()
+		return nil, nil, errors.New("build object store api value invalid")
+	}
+	if err := objs.GetError(); err != nil {
+		osRef.Release()
+		return nil, nil, err
+	}
+	return objs, osRef, nil
+}
