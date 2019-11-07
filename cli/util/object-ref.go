7c3add8b6cc771182637b37e946c26763bdeb8a6
Christian Stewart <christian@paral.in>
2019-11-06 17:55:14 -0800

    feat: add utility cli sub-commands
    Signed-off-by: Christian Stewart <christian@paral.in>

---
+++ cli/util/object-ref.go
@@ -0,0 +1,45 @@
+package cliutil
+
+import (
+	"errors"
+	"github.com/aperturerobotics/hydra/block/object"
+	ucli "github.com/urfave/cli"
+	"os"
+)
+
+// RunParseObjectRef parses the object ref provided.
+func (a *UtilArgs) RunParseObjectRef(cctx *ucli.Context) error {
+	if a.ObjectRef == "" {
+		return errors.New("object ref must be specified")
+	}
+
+	oref, err := object.ParseObjectRef(a.ObjectRef)
+	if err != nil {
+		return err
+	}
+
+	os.Stdout.WriteString("Bucket ID: ")
+	if obid := oref.GetBucketId(); obid != "" {
+		os.Stdout.WriteString(obid)
+	} else {
+		os.Stdout.WriteString("<none>")
+	}
+	os.Stdout.WriteString("\n")
+
+	os.Stdout.WriteString("Transform Config Ref: ")
+	if tcr := oref.GetTransformConfRef(); !tcr.GetEmpty() {
+		os.Stdout.WriteString(tcr.MarshalString())
+	} else {
+		os.Stdout.WriteString("<none>")
+	}
+	os.Stdout.WriteString("\n")
+
+	os.Stdout.WriteString("Root Ref: ")
+	if tcr := oref.GetRootRef(); !tcr.GetEmpty() {
+		os.Stdout.WriteString(tcr.MarshalString())
+	} else {
+		os.Stdout.WriteString("<none>")
+	}
+	os.Stdout.WriteString("\n")
+	return nil
+}
---
+++ cli/util/util.go
@@ -0,0 +1,70 @@
+package cliutil
+
+import (
+	"context"
+
+	"github.com/sirupsen/logrus"
+	ucli "github.com/urfave/cli"
+)
+
+// UtilArgs contains the utility arguments and functions.
+type UtilArgs struct {
+	// le is the logger entry
+	le *logrus.Entry
+	// ctx is the context
+	ctx context.Context
+
+	// ObjectRef is the object ref string.
+	ObjectRef string
+}
+
+// BuildFlags attaches the flags to a flag set.
+func (a *UtilArgs) BuildFlags() []ucli.Flag {
+	return []ucli.Flag{}
+}
+
+// BuildCommands attaches the commands.
+func (a *UtilArgs) BuildCommands() []ucli.Command {
+	return []ucli.Command{
+		ucli.Command{
+			Name:   "parse-object-ref",
+			Usage:  "parses an object.ObjectRef and prints information",
+			Action: a.RunParseObjectRef,
+			Flags: []ucli.Flag{
+				ucli.StringFlag{
+					Name:        "object-ref, r",
+					Usage:       "reference to parse",
+					Destination: &a.ObjectRef,
+				},
+			},
+		},
+	}
+}
+
+// SetContext sets the context.
+func (a *UtilArgs) SetContext(c context.Context) {
+	a.ctx = c
+}
+
+// GetContext returns the context.
+func (a *UtilArgs) GetContext() context.Context {
+	if c := a.ctx; c != nil {
+		return c
+	}
+	return context.TODO()
+}
+
+// SetLogger sets the root log entry.
+func (a *UtilArgs) SetLogger(le *logrus.Entry) {
+	a.le = le
+}
+
+// GetLogger returns the log entry
+func (a *UtilArgs) GetLogger() *logrus.Entry {
+	if le := a.le; le != nil {
+		return le
+	}
+	log := logrus.New()
+	log.SetLevel(logrus.DebugLevel)
+	return logrus.NewEntry(log)
+}
