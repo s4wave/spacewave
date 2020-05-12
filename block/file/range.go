46d658a774656aec9d3a9c8ed176eec00696a3ea
Christian Stewart <christian@paral.in>
2020-05-11 21:21:51 -0700

    fix: sort ranges properly
    Signed-off-by: Christian Stewart <christian@paral.in>

--- block/file/range.go
+++ block/file/range.go
@@ -2,6 +2,11 @@ package file
 
 // LessThanRange compares to ranges, returning true if r < other.
 func (r *Range) LessThanRange(other *Range) bool {
-	return r.GetStart() < other.GetStart() ||
-		r.GetNonce() < other.GetNonce()
+	// first check if the start is before
+	rs := r.GetStart()
+	os := other.GetStart()
+	if rs == os {
+		return r.GetNonce() < other.GetNonce()
+	}
+	return rs < os
 }
