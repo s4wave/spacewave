b4b236b9b48ffd6bf6a8685319efea23a4f06bb4
Christian Stewart <christian@paral.in>
2020-05-14 14:39:10 -0700

    fix: update traversal code for new sub-block features
    Signed-off-by: Christian Stewart <christian@paral.in>

--- block/traverse/traverse.go
+++ block/traverse/traverse.go
@@ -20,17 +20,17 @@ type Location struct {
 	Parent *Location
 	// Cursor contains the block graph cursor at the location.
 	Cursor *block.Cursor
-	// Block contains the block at the location.
+	// Block contains the block or sub-block at the location.
 	// May be nil if the block type is unknown or ref not found.
-	Block block.Block
+	Block interface{}
 	// ParentRefID is the reference ID that was previously followed.
 	ParentRefID uint32
 }
 
 // GetParentBlocks traverses the list and gets all parent blocks
 // The tail block is the first in the list.
-func (l *Location) GetParentBlocks() []block.Block {
-	res := make([]block.Block, 0, l.Depth)
+func (l *Location) GetParentBlocks() []interface{} {
+	res := make([]interface{}, 0, l.Depth)
 	for x := l; x != nil; x = x.Parent {
 		res = append(res, x.Block)
 	}
@@ -73,37 +73,37 @@ func visitRecursive(
 		return err
 	}
 	// follow each ref
+	refs, err := loc.Cursor.GetAllRefs()
+	if err != nil {
+		return errors.Wrap(err, "get block refs")
+	}
 	locBlock, locBlockOk := loc.Block.(block.BlockWithRefs)
-	if locBlockOk {
-		refs, err := loc.Cursor.GetAllRefs()
-		if err != nil {
-			return errors.Wrap(err, "get block refs")
+	for refID, refCs := range refs {
+		if refCs == nil {
+			continue
 		}
-		for refID, refCs := range refs {
-			if refCs == nil {
-				continue
-			}
+		refBlk, refBlkIsSubBlk := refCs.GetBlock()
+		if refBlk == nil && !refBlkIsSubBlk && locBlockOk {
 			blockRefCtor := locBlock.GetBlockRefCtor(refID)
-			var refBlk block.Block
 			if blockRefCtor != nil {
 				refBlk, err = refCs.Unmarshal(blockRefCtor)
 				if err != nil {
 					return errors.Wrapf(err, "follow ref %d", refID)
 				}
 			}
-			err = visitRecursive(ctx, &Location{
-				Depth:       loc.Depth + 1,
-				Parent:      loc,
-				Cursor:      refCs,
-				Block:       refBlk,
-				ParentRefID: refID,
-			}, cb)
-			if err != nil {
-				if err == ErrContinue {
-					continue
-				}
-				return err
+		}
+		err = visitRecursive(ctx, &Location{
+			Depth:       loc.Depth + 1,
+			Parent:      loc,
+			Cursor:      refCs,
+			Block:       refBlk,
+			ParentRefID: refID,
+		}, cb)
+		if err != nil {
+			if err == ErrContinue {
+				continue
 			}
+			return err
 		}
 	}
 	return nil
