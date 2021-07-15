83fa1d435aea86274ce66b1cc9dc91d69671c7cd
Christian Stewart <christian@paral.in>
2021-07-15 16:47:06 -0700

    feat(quad): add graph quad block
    Signed-off-by: Christian Stewart <christian@paral.in>

---
+++ block/quad/quad.go
@@ -0,0 +1,47 @@
+package quad
+
+import (
+	"github.com/aperturerobotics/hydra/block"
+	"github.com/golang/protobuf/proto"
+)
+
+// NewQuadBlock constructs a new object block.
+func NewQuadBlock() block.Block {
+	return &Quad{}
+}
+
+// IsEmpty checks if the graph quad is empty.
+// Considered empty if subject, predicate, or object fields are empty.
+func (o *Quad) IsEmpty() bool {
+	return o.GetSubject() == "" ||
+		o.GetPredicate() == "" ||
+		o.GetObj() == ""
+}
+
+// Clone clones the graph quad.
+func (o *Quad) Clone() *Quad {
+	if o == nil {
+		return nil
+	}
+	return &Quad{
+		Subject:   o.Subject,
+		Predicate: o.Predicate,
+		Obj:       o.Obj,
+		Label:     o.Label,
+	}
+}
+
+// MarshalBlock marshals the block to binary.
+// This is the initial step of marshaling, before transformations.
+func (o *Quad) MarshalBlock() ([]byte, error) {
+	return proto.Marshal(o)
+}
+
+// UnmarshalBlock unmarshals the block to the object.
+// This is the final step of decoding, after transformations.
+func (o *Quad) UnmarshalBlock(data []byte) error {
+	return proto.Unmarshal(data, o)
+}
+
+// _ is a type assertion
+var _ block.Block = ((*Quad)(nil))
