f9f24d076bbadcb1a04dc93963df6733d6615fc2
Christian Stewart <christian@paral.in>
2021-10-17 18:30:50 -0700

    refactor: move banner to common package
    Signed-off-by: Christian Stewart <christian@paral.in>

--- entrypoint/browser/banner.go
+++ entrypoint/browser/banner.go
@@ -6,26 +6,13 @@ package main
 import (
 	"runtime"
 	"syscall/js"
-)
 
-const apertureBanner = `
-       d8888                           888
-      d88888                           888                             
-     d88P888                           888                             
-    d88P 888 88888b.   .d88b.  888d888 888888 888  888 888d888 .d88b.  
-   d88P  888 888 "88b d8P  Y8b 888P"   888    888  888 888P"  d8P  Y8b 
-  d88P   888 888  888 88888888 888     888    888  888 888    88888888 
- d8888888888 888 d88P Y8b.     888     Y88b.  Y88b 888 888    Y8b.     
-d88P     888 88888P"   "Y8888  888      "Y888  "Y88888 888     "Y8888  
-             888                                                       
-             888                                                       
-             888     Welcome, user. `
+	"github.com/aperturerobotics/bldr/banner"
+)
 
 // formatBanner formats the full banner.
 func formatBanner() string {
-	// versionInfo is the version info str
-	versionInfo := "Bldr " + runtime.Version() + " on " + runtime.GOOS + "/" + runtime.GOARCH
-	return apertureBanner + versionInfo
+	return banner.FormatBanner()
 }
 
 // writeBanner writes the banner to the browser console.
