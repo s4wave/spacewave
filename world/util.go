b2ab74c6e3e76f93f353388cb20a4d7b963906b2
Christian Stewart <christian@paral.in>
2021-12-28 13:53:25 -0800

    world: add lookup root ref util
    Signed-off-by: Christian Stewart <christian@paral.in>

--- world/util.go
+++ world/util.go
@@ -1,6 +1,9 @@
 package world
 
-import "github.com/pkg/errors"
+import (
+	"github.com/aperturerobotics/hydra/bucket"
+	"github.com/pkg/errors"
+)
 
 // AssertObjectRev asserts that an object is at a given rev.
 func AssertObjectRev(obj ObjectState, expected uint64) error {
@@ -9,4 +12,25 @@ func AssertObjectRev(obj ObjectState, expected uint64) error {
 		err = errors.Wrapf(ErrUnexpectedRev, "expected %d got %d", expected, rev)
 	}
 	return err
+}
+
+// LookupRootRef gets an object and returns its root reference and rev.
+//
+// If not found, returns nil, 0, nil.
+func LookupRootRef(eng Engine, key string) (*bucket.ObjectRef, uint64, error) {
+	stx, err := eng.NewTransaction(false)
+	if err != nil {
+		return nil, 0, err
+	}
+	defer stx.Discard()
+
+	obj, found, err := stx.GetObject(key)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	if !found {
+		return nil, 0, nil
+	}
+	return obj.GetRootRef()
 }
