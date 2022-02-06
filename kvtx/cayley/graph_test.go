57db0139e2e4c7180f12e45055324515cee4d221
Christian Stewart <christian@paral.in>
2022-02-05 19:54:40 -0800

    chore(cayley): add more to the unit test
    Signed-off-by: Christian Stewart <christian@paral.in>

--- kvtx/cayley/graph_test.go
+++ kvtx/cayley/graph_test.go
@@ -1,12 +1,15 @@
 package kvtx_cayley
 
 import (
+	"context"
 	"testing"
 
 	kvtx_vlogger "github.com/aperturerobotics/hydra/kvtx/vlogger"
 	store_kvtx_inmem "github.com/aperturerobotics/hydra/store/kvtx/inmem"
 	"github.com/cayleygraph/cayley"
 	"github.com/cayleygraph/cayley/graph"
+	"github.com/cayleygraph/cayley/graph/iterator"
+	"github.com/cayleygraph/cayley/query/path"
 	"github.com/cayleygraph/quad"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
@@ -14,6 +17,7 @@ import (
 
 // TestCayleyGraph_Basic performs a basic cayley test.
 func TestCayleyGraph_Basic(t *testing.T) {
+	ctx := context.Background()
 	log := logrus.New()
 	log.SetLevel(logrus.DebugLevel)
 	le := logrus.NewEntry(log)
@@ -24,7 +28,7 @@ func TestCayleyGraph_Basic(t *testing.T) {
 	graphOptions := graph.Options{}
 	graph, err := NewGraph(objStore, graphOptions)
 	if err != nil {
-		panic(err)
+		t.Fatal(err.Error())
 	}
 
 	// graph is the cayley graph.
@@ -34,8 +38,10 @@ func TestCayleyGraph_Basic(t *testing.T) {
 	store.AddQuad(quad.Make("phrase of the day", "is of course", "Hello World!", nil))
 	store.AddQuad(quad.Make("phrase of the day", "is of course", "I like trains!", nil))
 
-	// Now we create the path, to get to our data
-	p := cayley.StartPath(store, quad.String("phrase of the day")).Out(quad.String("is of course"))
+	// Create path querying the data.
+	p := cayley.
+		StartPath(store, quad.String("phrase of the day")).
+		Out(quad.String("is of course"))
 
 	// Now we iterate over results. Arguments:
 	// 1. Optional context used for cancellation.
@@ -51,6 +57,38 @@ func TestCayleyGraph_Basic(t *testing.T) {
 		err = errors.Errorf("expected 2 values but got %d", nvals)
 	}
 	if err != nil {
-		panic(err)
+		t.Fatal(err.Error())
+	}
+
+	iterateShape := func(shape iterator.Shape) int {
+		itt := shape.Iterate()
+		var nm int
+		for itt.Next(ctx) {
+			nv, err := store.NameOf(itt.Result())
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+			t.Logf("value: %v", nv)
+			nm++
+		}
+		if err := itt.Err(); err != nil {
+			t.Fatal(err.Error())
+		}
+		return nm
+	}
+
+	// Test a path selecting all nodes in the db.
+	shape := store.NodesAllIterator()
+	nodesAllN := iterateShape(shape)
+
+	pshape := path.NewPath(store).Shape().BuildIterator(store)
+	shapeN := iterateShape(pshape)
+	if shapeN != nodesAllN {
+		t.Fatalf("got %d nodes", shapeN)
+	}
+
+	t.Logf("total matched nodes: %v", nodesAllN)
+	if nodesAllN != 4 {
+		t.Fatalf("expected 4 nodes but got %d", nodesAllN)
 	}
 }
