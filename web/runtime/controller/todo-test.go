c880f17839f20603727ea47e5ebfb791ce664584
Christian Stewart <christian@paral.in>
2022-10-04 19:13:18 -0700

    refactor: remove old test-component
    Signed-off-by: Christian Stewart <christian@paral.in>

--- web/runtime/controller/todo-test.go
+++ web/runtime/controller/todo-test.go
@@ -4,10 +4,8 @@ package web_runtime_controller
 
 import (
 	"context"
-	"net/http"
 	"time"
 
-	demo "github.com/aperturerobotics/bldr/toys/test-component"
 	"github.com/aperturerobotics/hydra/unixfs"
 	unixfs_world "github.com/aperturerobotics/hydra/unixfs/world"
 	world_testbed "github.com/aperturerobotics/hydra/world/testbed"
@@ -42,39 +40,16 @@ func buildExampleFS(ctx context.Context, le *logrus.Entry) (ufs *unixfs.FS, utb 
 	}
 
 	// create test script
-	err = billy_util.WriteFile(bfs, "test.js", []byte(getTestComponentJS()+"\n"), 0755)
-	if err != nil {
-		return nil, nil, err
-	}
+	/*
+		err = billy_util.WriteFile(bfs, "test.js", []byte(getTestComponentJS()+"\n"), 0755)
+		if err != nil {
+			return nil, nil, err
+		}
+	*/
 
 	// done
 	return ufs, utb, nil
 }
 
-func getTestComponentJS() string {
-	return demo.TestComponentJS
-}
-
 //go:embed test.png
 var demoPng []byte
-
-func getTestSwMux(le *logrus.Entry) *http.ServeMux {
-	mux := http.NewServeMux()
-	mux.Handle("/b/test.png", http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		le.Debugf("service worker fetch test png: %s", req.URL.String())
-		// TODO: Demo image
-		rw.Header().Set("Content-Type", "image/png")
-		rw.WriteHeader(200)
-		// basic test image
-		rw.Write(demoPng)
-	}))
-
-	// TODO DEMO
-	mux.Handle("/b/test.jsb", http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		le.Debugf("service worker fetch test component: %s", req.URL.String())
-		rw.Header().Set("Content-Type", "text/javascript")
-		rw.WriteHeader(200)
-		rw.Write([]byte(getTestComponentJS() + "\n"))
-	}))
-	return mux
-}
