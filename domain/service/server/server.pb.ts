289abff6b5eed5f503b272c7590ed4e15f1b8a66
Christian Stewart <christian@paral.in>
2022-12-02 03:52:43 -0800

    chore: update deps
    Signed-off-by: Christian Stewart <christian@paral.in>

--- domain/service/server/server.pb.ts
+++ domain/service/server/server.pb.ts
@@ -1,8 +1,8 @@
 /* eslint-disable */
-import Long from "long";
-import _m0 from "protobufjs/minimal.js";
+import Long from 'long'
+import _m0 from 'protobufjs/minimal.js'
 
-export const protobufPackage = "identity.domain.server";
+export const protobufPackage = 'identity.domain.server'
 
 /**
  * Config configures the identity server.
@@ -13,70 +13,73 @@ export interface Config {
    * PeerIds are the list of peer IDs to listen on.
    * If empty, allows any incoming peer id w/ the protocol id.
    */
-  peerIds: string[];
+  peerIds: string[]
   /**
    * DomainIds is the list of domain IDs to service.
    * If empty, allows any domain ID.
    */
-  domainIds: string[];
+  domainIds: string[]
   /** RequestTimeout limits the amount of time a request can take. */
-  requestTimeout: string;
+  requestTimeout: string
 }
 
 function createBaseConfig(): Config {
-  return { peerIds: [], domainIds: [], requestTimeout: "" };
+  return { peerIds: [], domainIds: [], requestTimeout: '' }
 }
 
 export const Config = {
-  encode(message: Config, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+  encode(
+    message: Config,
+    writer: _m0.Writer = _m0.Writer.create()
+  ): _m0.Writer {
     for (const v of message.peerIds) {
-      writer.uint32(10).string(v!);
+      writer.uint32(10).string(v!)
     }
     for (const v of message.domainIds) {
-      writer.uint32(18).string(v!);
+      writer.uint32(18).string(v!)
     }
-    if (message.requestTimeout !== "") {
-      writer.uint32(26).string(message.requestTimeout);
+    if (message.requestTimeout !== '') {
+      writer.uint32(26).string(message.requestTimeout)
     }
-    return writer;
+    return writer
   },
 
   decode(input: _m0.Reader | Uint8Array, length?: number): Config {
-    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
-    let end = length === undefined ? reader.len : reader.pos + length;
-    const message = createBaseConfig();
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input)
+    let end = length === undefined ? reader.len : reader.pos + length
+    const message = createBaseConfig()
     while (reader.pos < end) {
-      const tag = reader.uint32();
+      const tag = reader.uint32()
       switch (tag >>> 3) {
         case 1:
-          message.peerIds.push(reader.string());
-          break;
+          message.peerIds.push(reader.string())
+          break
         case 2:
-          message.domainIds.push(reader.string());
-          break;
+          message.domainIds.push(reader.string())
+          break
         case 3:
-          message.requestTimeout = reader.string();
-          break;
+          message.requestTimeout = reader.string()
+          break
         default:
-          reader.skipType(tag & 7);
-          break;
+          reader.skipType(tag & 7)
+          break
       }
     }
-    return message;
+    return message
   },
 
   // encodeTransform encodes a source of message objects.
   // Transform<Config, Uint8Array>
   async *encodeTransform(
-    source: AsyncIterable<Config | Config[]> | Iterable<Config | Config[]>,
+    source: AsyncIterable<Config | Config[]> | Iterable<Config | Config[]>
   ): AsyncIterable<Uint8Array> {
     for await (const pkt of source) {
       if (Array.isArray(pkt)) {
         for (const p of pkt) {
-          yield* [Config.encode(p).finish()];
+          yield* [Config.encode(p).finish()]
         }
       } else {
-        yield* [Config.encode(pkt).finish()];
+        yield* [Config.encode(pkt).finish()]
       }
     }
   },
@@ -84,70 +87,98 @@ export const Config = {
   // decodeTransform decodes a source of encoded messages.
   // Transform<Uint8Array, Config>
   async *decodeTransform(
-    source: AsyncIterable<Uint8Array | Uint8Array[]> | Iterable<Uint8Array | Uint8Array[]>,
+    source:
+      | AsyncIterable<Uint8Array | Uint8Array[]>
+      | Iterable<Uint8Array | Uint8Array[]>
   ): AsyncIterable<Config> {
     for await (const pkt of source) {
       if (Array.isArray(pkt)) {
         for (const p of pkt) {
-          yield* [Config.decode(p)];
+          yield* [Config.decode(p)]
         }
       } else {
-        yield* [Config.decode(pkt)];
+        yield* [Config.decode(pkt)]
       }
     }
   },
 
   fromJSON(object: any): Config {
     return {
-      peerIds: Array.isArray(object?.peerIds) ? object.peerIds.map((e: any) => String(e)) : [],
-      domainIds: Array.isArray(object?.domainIds) ? object.domainIds.map((e: any) => String(e)) : [],
-      requestTimeout: isSet(object.requestTimeout) ? String(object.requestTimeout) : "",
-    };
+      peerIds: Array.isArray(object?.peerIds)
+        ? object.peerIds.map((e: any) => String(e))
+        : [],
+      domainIds: Array.isArray(object?.domainIds)
+        ? object.domainIds.map((e: any) => String(e))
+        : [],
+      requestTimeout: isSet(object.requestTimeout)
+        ? String(object.requestTimeout)
+        : '',
+    }
   },
 
   toJSON(message: Config): unknown {
-    const obj: any = {};
+    const obj: any = {}
     if (message.peerIds) {
-      obj.peerIds = message.peerIds.map((e) => e);
+      obj.peerIds = message.peerIds.map((e) => e)
     } else {
-      obj.peerIds = [];
+      obj.peerIds = []
     }
     if (message.domainIds) {
-      obj.domainIds = message.domainIds.map((e) => e);
+      obj.domainIds = message.domainIds.map((e) => e)
     } else {
-      obj.domainIds = [];
+      obj.domainIds = []
     }
-    message.requestTimeout !== undefined && (obj.requestTimeout = message.requestTimeout);
-    return obj;
+    message.requestTimeout !== undefined &&
+      (obj.requestTimeout = message.requestTimeout)
+    return obj
   },
 
   fromPartial<I extends Exact<DeepPartial<Config>, I>>(object: I): Config {
-    const message = createBaseConfig();
-    message.peerIds = object.peerIds?.map((e) => e) || [];
-    message.domainIds = object.domainIds?.map((e) => e) || [];
-    message.requestTimeout = object.requestTimeout ?? "";
-    return message;
+    const message = createBaseConfig()
+    message.peerIds = object.peerIds?.map((e) => e) || []
+    message.domainIds = object.domainIds?.map((e) => e) || []
+    message.requestTimeout = object.requestTimeout ?? ''
+    return message
   },
-};
+}
 
-type Builtin = Date | Function | Uint8Array | string | number | boolean | undefined;
+type Builtin =
+  | Date
+  | Function
+  | Uint8Array
+  | string
+  | number
+  | boolean
+  | undefined
 
-export type DeepPartial<T> = T extends Builtin ? T
-  : T extends Long ? string | number | Long : T extends Array<infer U> ? Array<DeepPartial<U>>
-  : T extends ReadonlyArray<infer U> ? ReadonlyArray<DeepPartial<U>>
-  : T extends { $case: string } ? { [K in keyof Omit<T, "$case">]?: DeepPartial<T[K]> } & { $case: T["$case"] }
-  : T extends {} ? { [K in keyof T]?: DeepPartial<T[K]> }
-  : Partial<T>;
+export type DeepPartial<T> = T extends Builtin
+  ? T
+  : T extends Long
+  ? string | number | Long
+  : T extends Array<infer U>
+  ? Array<DeepPartial<U>>
+  : T extends ReadonlyArray<infer U>
+  ? ReadonlyArray<DeepPartial<U>>
+  : T extends { $case: string }
+  ? { [K in keyof Omit<T, '$case'>]?: DeepPartial<T[K]> } & {
+      $case: T['$case']
+    }
+  : T extends {}
+  ? { [K in keyof T]?: DeepPartial<T[K]> }
+  : Partial<T>
 
-type KeysOfUnion<T> = T extends T ? keyof T : never;
-export type Exact<P, I extends P> = P extends Builtin ? P
-  : P & { [K in keyof P]: Exact<P[K], I[K]> } & { [K in Exclude<keyof I, KeysOfUnion<P>>]: never };
+type KeysOfUnion<T> = T extends T ? keyof T : never
+export type Exact<P, I extends P> = P extends Builtin
+  ? P
+  : P & { [K in keyof P]: Exact<P[K], I[K]> } & {
+      [K in Exclude<keyof I, KeysOfUnion<P>>]: never
+    }
 
 if (_m0.util.Long !== Long) {
-  _m0.util.Long = Long as any;
-  _m0.configure();
+  _m0.util.Long = Long as any
+  _m0.configure()
 }
 
 function isSet(value: any): boolean {
-  return value !== null && value !== undefined;
+  return value !== null && value !== undefined
 }
--- world/world.pb.ts
+++ world/world.pb.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
-import { ObjectRef } from "@go/github.com/aperturerobotics/hydra/bucket/bucket.pb.js";
-import Long from "long";
-import _m0 from "protobufjs/minimal.js";
+import { ObjectRef } from '@go/github.com/aperturerobotics/hydra/bucket/bucket.pb.js'
+import Long from 'long'
+import _m0 from 'protobufjs/minimal.js'
 
-export const protobufPackage = "identity.world";
+export const protobufPackage = 'identity.world'
 
 /**
  * EntityUpdateOp updates an entity and links to other objects.
@@ -14,63 +14,68 @@ export const protobufPackage = "identity.world";
  */
 export interface EntityUpdateOp {
   /** EntityRef is the reference to the latest Entity object. */
-  entityRef: ObjectRef | undefined;
+  entityRef: ObjectRef | undefined
 }
 
 /** KeypairUpdateOp updates a Keypair. */
 export interface KeypairUpdateOp {
   /** KeypairRef is the reference to the Keypair object. */
-  keypairRef: ObjectRef | undefined;
+  keypairRef: ObjectRef | undefined
 }
 
 /** DomainInfoUpdateOp updates a DomainInfo. */
 export interface DomainInfoUpdateOp {
   /** DomainInfoRef is the reference to the DomainInfo object. */
-  domainInfoRef: ObjectRef | undefined;
+  domainInfoRef: ObjectRef | undefined
 }
 
 function createBaseEntityUpdateOp(): EntityUpdateOp {
-  return { entityRef: undefined };
+  return { entityRef: undefined }
 }
 
 export const EntityUpdateOp = {
-  encode(message: EntityUpdateOp, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+  encode(
+    message: EntityUpdateOp,
+    writer: _m0.Writer = _m0.Writer.create()
+  ): _m0.Writer {
     if (message.entityRef !== undefined) {
-      ObjectRef.encode(message.entityRef, writer.uint32(10).fork()).ldelim();
+      ObjectRef.encode(message.entityRef, writer.uint32(10).fork()).ldelim()
     }
-    return writer;
+    return writer
   },
 
   decode(input: _m0.Reader | Uint8Array, length?: number): EntityUpdateOp {
-    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
-    let end = length === undefined ? reader.len : reader.pos + length;
-    const message = createBaseEntityUpdateOp();
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input)
+    let end = length === undefined ? reader.len : reader.pos + length
+    const message = createBaseEntityUpdateOp()
     while (reader.pos < end) {
-      const tag = reader.uint32();
+      const tag = reader.uint32()
       switch (tag >>> 3) {
         case 1:
-          message.entityRef = ObjectRef.decode(reader, reader.uint32());
-          break;
+          message.entityRef = ObjectRef.decode(reader, reader.uint32())
+          break
         default:
-          reader.skipType(tag & 7);
-          break;
+          reader.skipType(tag & 7)
+          break
       }
     }
-    return message;
+    return message
   },
 
   // encodeTransform encodes a source of message objects.
   // Transform<EntityUpdateOp, Uint8Array>
   async *encodeTransform(
-    source: AsyncIterable<EntityUpdateOp | EntityUpdateOp[]> | Iterable<EntityUpdateOp | EntityUpdateOp[]>,
+    source:
+      | AsyncIterable<EntityUpdateOp | EntityUpdateOp[]>
+      | Iterable<EntityUpdateOp | EntityUpdateOp[]>
   ): AsyncIterable<Uint8Array> {
     for await (const pkt of source) {
       if (Array.isArray(pkt)) {
         for (const p of pkt) {
-          yield* [EntityUpdateOp.encode(p).finish()];
+          yield* [EntityUpdateOp.encode(p).finish()]
         }
       } else {
-        yield* [EntityUpdateOp.encode(pkt).finish()];
+        yield* [EntityUpdateOp.encode(pkt).finish()]
       }
     }
   },
@@ -78,81 +83,97 @@ export const EntityUpdateOp = {
   // decodeTransform decodes a source of encoded messages.
   // Transform<Uint8Array, EntityUpdateOp>
   async *decodeTransform(
-    source: AsyncIterable<Uint8Array | Uint8Array[]> | Iterable<Uint8Array | Uint8Array[]>,
+    source:
+      | AsyncIterable<Uint8Array | Uint8Array[]>
+      | Iterable<Uint8Array | Uint8Array[]>
   ): AsyncIterable<EntityUpdateOp> {
     for await (const pkt of source) {
       if (Array.isArray(pkt)) {
         for (const p of pkt) {
-          yield* [EntityUpdateOp.decode(p)];
+          yield* [EntityUpdateOp.decode(p)]
         }
       } else {
-        yield* [EntityUpdateOp.decode(pkt)];
+        yield* [EntityUpdateOp.decode(pkt)]
       }
     }
   },
 
   fromJSON(object: any): EntityUpdateOp {
-    return { entityRef: isSet(object.entityRef) ? ObjectRef.fromJSON(object.entityRef) : undefined };
+    return {
+      entityRef: isSet(object.entityRef)
+        ? ObjectRef.fromJSON(object.entityRef)
+        : undefined,
+    }
   },
 
   toJSON(message: EntityUpdateOp): unknown {
-    const obj: any = {};
+    const obj: any = {}
     message.entityRef !== undefined &&
-      (obj.entityRef = message.entityRef ? ObjectRef.toJSON(message.entityRef) : undefined);
-    return obj;
+      (obj.entityRef = message.entityRef
+        ? ObjectRef.toJSON(message.entityRef)
+        : undefined)
+    return obj
   },
 
-  fromPartial<I extends Exact<DeepPartial<EntityUpdateOp>, I>>(object: I): EntityUpdateOp {
-    const message = createBaseEntityUpdateOp();
-    message.entityRef = (object.entityRef !== undefined && object.entityRef !== null)
-      ? ObjectRef.fromPartial(object.entityRef)
-      : undefined;
-    return message;
+  fromPartial<I extends Exact<DeepPartial<EntityUpdateOp>, I>>(
+    object: I
+  ): EntityUpdateOp {
+    const message = createBaseEntityUpdateOp()
+    message.entityRef =
+      object.entityRef !== undefined && object.entityRef !== null
+        ? ObjectRef.fromPartial(object.entityRef)
+        : undefined
+    return message
   },
-};
+}
 
 function createBaseKeypairUpdateOp(): KeypairUpdateOp {
-  return { keypairRef: undefined };
+  return { keypairRef: undefined }
 }
 
 export const KeypairUpdateOp = {
-  encode(message: KeypairUpdateOp, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+  encode(
+    message: KeypairUpdateOp,
+    writer: _m0.Writer = _m0.Writer.create()
+  ): _m0.Writer {
     if (message.keypairRef !== undefined) {
-      ObjectRef.encode(message.keypairRef, writer.uint32(10).fork()).ldelim();
+      ObjectRef.encode(message.keypairRef, writer.uint32(10).fork()).ldelim()
     }
-    return writer;
+    return writer
   },
 
   decode(input: _m0.Reader | Uint8Array, length?: number): KeypairUpdateOp {
-    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
-    let end = length === undefined ? reader.len : reader.pos + length;
-    const message = createBaseKeypairUpdateOp();
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input)
+    let end = length === undefined ? reader.len : reader.pos + length
+    const message = createBaseKeypairUpdateOp()
     while (reader.pos < end) {
-      const tag = reader.uint32();
+      const tag = reader.uint32()
       switch (tag >>> 3) {
         case 1:
-          message.keypairRef = ObjectRef.decode(reader, reader.uint32());
-          break;
+          message.keypairRef = ObjectRef.decode(reader, reader.uint32())
+          break
         default:
-          reader.skipType(tag & 7);
-          break;
+          reader.skipType(tag & 7)
+          break
       }
     }
-    return message;
+    return message
   },
 
   // encodeTransform encodes a source of message objects.
   // Transform<KeypairUpdateOp, Uint8Array>
   async *encodeTransform(
-    source: AsyncIterable<KeypairUpdateOp | KeypairUpdateOp[]> | Iterable<KeypairUpdateOp | KeypairUpdateOp[]>,
+    source:
+      | AsyncIterable<KeypairUpdateOp | KeypairUpdateOp[]>
+      | Iterable<KeypairUpdateOp | KeypairUpdateOp[]>
   ): AsyncIterable<Uint8Array> {
     for await (const pkt of source) {
       if (Array.isArray(pkt)) {
         for (const p of pkt) {
-          yield* [KeypairUpdateOp.encode(p).finish()];
+          yield* [KeypairUpdateOp.encode(p).finish()]
         }
       } else {
-        yield* [KeypairUpdateOp.encode(pkt).finish()];
+        yield* [KeypairUpdateOp.encode(pkt).finish()]
       }
     }
   },
@@ -160,67 +181,81 @@ export const KeypairUpdateOp = {
   // decodeTransform decodes a source of encoded messages.
   // Transform<Uint8Array, KeypairUpdateOp>
   async *decodeTransform(
-    source: AsyncIterable<Uint8Array | Uint8Array[]> | Iterable<Uint8Array | Uint8Array[]>,
+    source:
+      | AsyncIterable<Uint8Array | Uint8Array[]>
+      | Iterable<Uint8Array | Uint8Array[]>
   ): AsyncIterable<KeypairUpdateOp> {
     for await (const pkt of source) {
       if (Array.isArray(pkt)) {
         for (const p of pkt) {
-          yield* [KeypairUpdateOp.decode(p)];
+          yield* [KeypairUpdateOp.decode(p)]
         }
       } else {
-        yield* [KeypairUpdateOp.decode(pkt)];
+        yield* [KeypairUpdateOp.decode(pkt)]
       }
     }
   },
 
   fromJSON(object: any): KeypairUpdateOp {
-    return { keypairRef: isSet(object.keypairRef) ? ObjectRef.fromJSON(object.keypairRef) : undefined };
+    return {
+      keypairRef: isSet(object.keypairRef)
+        ? ObjectRef.fromJSON(object.keypairRef)
+        : undefined,
+    }
   },
 
   toJSON(message: KeypairUpdateOp): unknown {
-    const obj: any = {};
+    const obj: any = {}
     message.keypairRef !== undefined &&
-      (obj.keypairRef = message.keypairRef ? ObjectRef.toJSON(message.keypairRef) : undefined);
-    return obj;
+      (obj.keypairRef = message.keypairRef
+        ? ObjectRef.toJSON(message.keypairRef)
+        : undefined)
+    return obj
   },
 
-  fromPartial<I extends Exact<DeepPartial<KeypairUpdateOp>, I>>(object: I): KeypairUpdateOp {
-    const message = createBaseKeypairUpdateOp();
-    message.keypairRef = (object.keypairRef !== undefined && object.keypairRef !== null)
-      ? ObjectRef.fromPartial(object.keypairRef)
-      : undefined;
-    return message;
+  fromPartial<I extends Exact<DeepPartial<KeypairUpdateOp>, I>>(
+    object: I
+  ): KeypairUpdateOp {
+    const message = createBaseKeypairUpdateOp()
+    message.keypairRef =
+      object.keypairRef !== undefined && object.keypairRef !== null
+        ? ObjectRef.fromPartial(object.keypairRef)
+        : undefined
+    return message
   },
-};
+}
 
 function createBaseDomainInfoUpdateOp(): DomainInfoUpdateOp {
-  return { domainInfoRef: undefined };
+  return { domainInfoRef: undefined }
 }
 
 export const DomainInfoUpdateOp = {
-  encode(message: DomainInfoUpdateOp, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+  encode(
+    message: DomainInfoUpdateOp,
+    writer: _m0.Writer = _m0.Writer.create()
+  ): _m0.Writer {
     if (message.domainInfoRef !== undefined) {
-      ObjectRef.encode(message.domainInfoRef, writer.uint32(10).fork()).ldelim();
+      ObjectRef.encode(message.domainInfoRef, writer.uint32(10).fork()).ldelim()
     }
-    return writer;
+    return writer
   },
 
   decode(input: _m0.Reader | Uint8Array, length?: number): DomainInfoUpdateOp {
-    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
-    let end = length === undefined ? reader.len : reader.pos + length;
-    const message = createBaseDomainInfoUpdateOp();
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input)
+    let end = length === undefined ? reader.len : reader.pos + length
+    const message = createBaseDomainInfoUpdateOp()
     while (reader.pos < end) {
-      const tag = reader.uint32();
+      const tag = reader.uint32()
       switch (tag >>> 3) {
         case 1:
-          message.domainInfoRef = ObjectRef.decode(reader, reader.uint32());
-          break;
+          message.domainInfoRef = ObjectRef.decode(reader, reader.uint32())
+          break
         default:
-          reader.skipType(tag & 7);
-          break;
+          reader.skipType(tag & 7)
+          break
       }
     }
-    return message;
+    return message
   },
 
   // encodeTransform encodes a source of message objects.
@@ -228,15 +263,15 @@ export const DomainInfoUpdateOp = {
   async *encodeTransform(
     source:
       | AsyncIterable<DomainInfoUpdateOp | DomainInfoUpdateOp[]>
-      | Iterable<DomainInfoUpdateOp | DomainInfoUpdateOp[]>,
+      | Iterable<DomainInfoUpdateOp | DomainInfoUpdateOp[]>
   ): AsyncIterable<Uint8Array> {
     for await (const pkt of source) {
       if (Array.isArray(pkt)) {
         for (const p of pkt) {
-          yield* [DomainInfoUpdateOp.encode(p).finish()];
+          yield* [DomainInfoUpdateOp.encode(p).finish()]
         }
       } else {
-        yield* [DomainInfoUpdateOp.encode(pkt).finish()];
+        yield* [DomainInfoUpdateOp.encode(pkt).finish()]
       }
     }
   },
@@ -244,57 +279,87 @@ export const DomainInfoUpdateOp = {
   // decodeTransform decodes a source of encoded messages.
   // Transform<Uint8Array, DomainInfoUpdateOp>
   async *decodeTransform(
-    source: AsyncIterable<Uint8Array | Uint8Array[]> | Iterable<Uint8Array | Uint8Array[]>,
+    source:
+      | AsyncIterable<Uint8Array | Uint8Array[]>
+      | Iterable<Uint8Array | Uint8Array[]>
   ): AsyncIterable<DomainInfoUpdateOp> {
     for await (const pkt of source) {
       if (Array.isArray(pkt)) {
         for (const p of pkt) {
-          yield* [DomainInfoUpdateOp.decode(p)];
+          yield* [DomainInfoUpdateOp.decode(p)]
         }
       } else {
-        yield* [DomainInfoUpdateOp.decode(pkt)];
+        yield* [DomainInfoUpdateOp.decode(pkt)]
       }
     }
   },
 
   fromJSON(object: any): DomainInfoUpdateOp {
-    return { domainInfoRef: isSet(object.domainInfoRef) ? ObjectRef.fromJSON(object.domainInfoRef) : undefined };
+    return {
+      domainInfoRef: isSet(object.domainInfoRef)
+        ? ObjectRef.fromJSON(object.domainInfoRef)
+        : undefined,
+    }
   },
 
   toJSON(message: DomainInfoUpdateOp): unknown {
-    const obj: any = {};
+    const obj: any = {}
     message.domainInfoRef !== undefined &&
-      (obj.domainInfoRef = message.domainInfoRef ? ObjectRef.toJSON(message.domainInfoRef) : undefined);
-    return obj;
+      (obj.domainInfoRef = message.domainInfoRef
+        ? ObjectRef.toJSON(message.domainInfoRef)
+        : undefined)
+    return obj
   },
 
-  fromPartial<I extends Exact<DeepPartial<DomainInfoUpdateOp>, I>>(object: I): DomainInfoUpdateOp {
-    const message = createBaseDomainInfoUpdateOp();
-    message.domainInfoRef = (object.domainInfoRef !== undefined && object.domainInfoRef !== null)
-      ? ObjectRef.fromPartial(object.domainInfoRef)
-      : undefined;
-    return message;
+  fromPartial<I extends Exact<DeepPartial<DomainInfoUpdateOp>, I>>(
+    object: I
+  ): DomainInfoUpdateOp {
+    const message = createBaseDomainInfoUpdateOp()
+    message.domainInfoRef =
+      object.domainInfoRef !== undefined && object.domainInfoRef !== null
+        ? ObjectRef.fromPartial(object.domainInfoRef)
+        : undefined
+    return message
   },
-};
+}
 
-type Builtin = Date | Function | Uint8Array | string | number | boolean | undefined;
+type Builtin =
+  | Date
+  | Function
+  | Uint8Array
+  | string
+  | number
+  | boolean
+  | undefined
 
-export type DeepPartial<T> = T extends Builtin ? T
-  : T extends Long ? string | number | Long : T extends Array<infer U> ? Array<DeepPartial<U>>
-  : T extends ReadonlyArray<infer U> ? ReadonlyArray<DeepPartial<U>>
-  : T extends { $case: string } ? { [K in keyof Omit<T, "$case">]?: DeepPartial<T[K]> } & { $case: T["$case"] }
-  : T extends {} ? { [K in keyof T]?: DeepPartial<T[K]> }
-  : Partial<T>;
+export type DeepPartial<T> = T extends Builtin
+  ? T
+  : T extends Long
+  ? string | number | Long
+  : T extends Array<infer U>
+  ? Array<DeepPartial<U>>
+  : T extends ReadonlyArray<infer U>
+  ? ReadonlyArray<DeepPartial<U>>
+  : T extends { $case: string }
+  ? { [K in keyof Omit<T, '$case'>]?: DeepPartial<T[K]> } & {
+      $case: T['$case']
+    }
+  : T extends {}
+  ? { [K in keyof T]?: DeepPartial<T[K]> }
+  : Partial<T>
 
-type KeysOfUnion<T> = T extends T ? keyof T : never;
-export type Exact<P, I extends P> = P extends Builtin ? P
-  : P & { [K in keyof P]: Exact<P[K], I[K]> } & { [K in Exclude<keyof I, KeysOfUnion<P>>]: never };
+type KeysOfUnion<T> = T extends T ? keyof T : never
+export type Exact<P, I extends P> = P extends Builtin
+  ? P
+  : P & { [K in keyof P]: Exact<P[K], I[K]> } & {
+      [K in Exclude<keyof I, KeysOfUnion<P>>]: never
+    }
 
 if (_m0.util.Long !== Long) {
-  _m0.util.Long = Long as any;
-  _m0.configure();
+  _m0.util.Long = Long as any
+  _m0.configure()
 }
 
 function isSet(value: any): boolean {
-  return value !== null && value !== undefined;
+  return value !== null && value !== undefined
 }
