544c1f27aa9ac7f57ca64c2c6682e992cb4b1b19
Christian Stewart <christian@paral.in>
2022-12-24 03:08:11 -0800

    fix(kvtx): inmem: optimize in-mem map with xxhash
    Signed-off-by: Christian Stewart <christian@paral.in>

--- store/kvtx/inmem/inmem.go
+++ store/kvtx/inmem/inmem.go
@@ -4,42 +4,66 @@ import (
 	"context"
 	"sync"
 
-	"github.com/Workiva/go-datastructures/trie/ctrie"
 	"github.com/aperturerobotics/hydra/kvtx"
+	"github.com/aperturerobotics/util/broadcast"
 )
 
 // Store is a in-memory key-value store.
 //
 // Uses a fast ctrie (concurrent trie) K/V map.
 type Store struct {
-	// mtx guards ct
+	// m is the map containing the store
+	// key is encoded with base58
+	m map[uint64]valType
+
+	// bcast is broadcast when below fields change
+	bcast broadcast.Broadcast
+	// mtx guards the below fields
 	mtx sync.Mutex
-	// ct is the ctrie backing the store
-	ct *ctrie.Ctrie
-	// writeMtx guards write transactions
-	writeMtx sync.Mutex
+	// nreaders is the number of active readers
+	nreaders int
+	// writing indicates there's a write tx active
+	writing bool
+	// writeWaiting indicates a write tx is waiting
+	writeWaiting bool
 }
 
 // NewStore constructs a new key-value store.
 func NewStore() *Store {
-	return &Store{ct: ctrie.New(nil)}
+	return &Store{m: map[uint64]valType{}}
 }
 
 // NewTransaction returns a new transaction against the store.
 // Indicate write if the transaction will not be read-only.
 // Always call Discard() after you are done with the transaction.
 func (s *Store) NewTransaction(write bool) (kvtx.Tx, error) {
-	s.mtx.Lock()
-	c := s.ct
-	s.mtx.Unlock()
-	if write {
-		s.writeMtx.Lock()
-		c = c.Snapshot()
-	} else {
-		c = c.ReadOnlySnapshot()
+	for {
+		var tx kvtx.Tx
+		s.mtx.Lock()
+		if write {
+			if s.nreaders != 0 || s.writing {
+				s.writeWaiting = true
+			} else {
+				s.writing = true
+				s.writeWaiting = false
+				tx = newTx(s, true)
+			}
+		} else {
+			if !s.writing && !s.writeWaiting {
+				s.nreaders++
+				tx = newTx(s, false)
+			}
+		}
+		var bcastCh <-chan struct{}
+		if tx == nil {
+			bcastCh = s.bcast.GetWaitCh()
+		}
+		s.mtx.Unlock()
+		if tx != nil {
+			return tx, nil
+		}
+		<-bcastCh
 	}
-
-	return newTx(s, write, c), nil
 }
 
 // Execute executes the given store.
---
+++ store/kvtx/inmem/storage.go
@@ -0,0 +1,17 @@
+package store_kvtx_inmem
+
+import "github.com/cespare/xxhash"
+
+// keyType is the type used for keys.
+type keyType uint64
+
+// valType is the type used for values
+type valType struct {
+	key []byte
+	val []byte
+}
+
+// hashKey hashes the key.
+func hashKey(key []byte) uint64 {
+	return xxhash.Sum64(key)
+}
