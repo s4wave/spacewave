39a392638d396337a568f312136b61d3a63379a4
Christian Stewart <christian@paral.in>
2023-01-21 22:26:54 -0800

    refactor: use BldrComponent for example
    Signed-off-by: Christian Stewart <christian@paral.in>

--- example/example.tsx
+++ example/example.tsx
@@ -1,61 +1,49 @@
 import React from 'react'
 
-import { BldrContext } from '@bldr/web/bldr-react/bldr-context.js'
+import { createFunctionComponent, BldrComponent } from '@bldr/web/bldr-react'
+import { retryWithAbort } from '@bldr/web/bldr'
 import { EchoerClientImpl } from '@go/github.com/aperturerobotics/starpc/echo/index.js'
-import { WebDocument as BldrWebDocument } from '@bldr/web/bldr/web-document.js'
-import { WebView as BldrWebView } from '@bldr/web/bldr/web-view.js'
-import { createFunctionComponent } from '@bldr/web/bldr-react/function-component.js'
 
-import './example.css';
+import './example.css'
 
 // IExampleState contains state for Example.
 interface IExampleState {
-    message?: string
+  message?: string
 }
 
-class Example extends React.Component<{}, IExampleState> {
-    // context is the webDocument context
-    declare context: React.ContextType<typeof BldrContext>
-    static contextType = BldrContext
+class Example extends BldrComponent<{}, IExampleState> {
+  // echoHost is the echo service running on the plugin host.
+  private echoHost?: EchoerClientImpl
 
-    // echoHost is the echo service running on the plugin host.
-    private echoHost?: EchoerClientImpl
+  constructor(props: {}) {
+    super(props)
+    this.state = {}
+  }
 
-    constructor(props: {}) {
-        super(props)
-        this.state = {}
-    }
+  public componentDidMount() {
+    this.echoHost = new EchoerClientImpl(this.buildWebViewHostClient())
+    retryWithAbort(this.closeController.signal, this.runEchoRpc.bind(this), {
+      errorCb: (err) => {
+        console.warn('example Echo failed', err)
+      },
+    })
+  }
 
-    // webDocument exposes the web document from context.
-    get webDocument(): BldrWebDocument {
-        return this.context!.webDocument!
-    }
+  // runEchoRpc runs the echo rpc and updates the state.
+  private async runEchoRpc(): Promise<void> {
+    const resp = await this.echoHost?.Echo({
+      body: 'Hello from TypeScript via RPC round-trip to the plugin!',
+    })
+    this.setState({ message: resp?.body })
+  }
 
-    // webView exposes the web view from context.
-    get webView(): BldrWebView {
-        return this.context!.webView!
-    }
-
-    public componentDidMount() {
-        this.echoHost = new EchoerClientImpl(
-            this.webDocument.buildWebViewHostClient(this.webView.getUuid())
-        )
-        this._runEchoRpc()
-    }
-
-    // _runEchoRpc runs the echo rpc and updates the state.
-    private async _runEchoRpc(): Promise<void> {
-        const resp = await this.echoHost?.Echo({ body: 'Hello from TypeScript via RPC round-trip to the plugin!' })
-        this.setState({message: resp?.body})
-    }
-
-    public render() {
-        return (
-            <div className='example-message'>
-                {this.state.message || 'Loading...'}
-            </div>
-        )
-    }
+  public render() {
+    return (
+      <div className="example-message">
+        {this.state.message || 'Loading...'}
+      </div>
+    )
+  }
 }
 
 // Example will be constructed when the component is loaded.
