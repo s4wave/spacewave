ef9c89ba351de2be90c5113f5484cf3ea0c77588
Christian Stewart <christian@aperture.us>
2023-05-08 18:57:02 -0700

    fix: windows pipesock pipe name format
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- util/pipesock/windows.go
+++ util/pipesock/windows.go
@@ -6,26 +6,34 @@ package pipesock
 import (
 	"context"
 	"net"
-	"path/filepath"
+	"strings"
 
 	"github.com/Microsoft/go-winio"
+	b58 "github.com/mr-tron/base58/base58"
 	"github.com/sirupsen/logrus"
+	"github.com/zeebo/blake3"
 )
 
 // BuildPipeListener builds the pipe listener in the directory.
 func BuildPipeListener(le *logrus.Entry, rootDir, pipeUuid string) (net.Listener, error) {
-	pipeName := filepath.Join(rootDir, ".pipe", pipeUuid)
-	le.Debugf("listening on winio socket: %s", pipeUuid)
+	pipeName := BuildPipeName(rootDir, pipeUuid)
+	le.Debugf("listening on winio pipe: %s", pipeName)
 	return winio.ListenPipe(pipeName, nil)
 }
 
 // DialPipeListener connects to the pipe listener in the directory.
 func DialPipeListener(ctx context.Context, le *logrus.Entry, rootDir, pipeUuid string) (net.Conn, error) {
-	pipePath := filepath.Join(rootDir, ".pipe-"+pipeUuid)
-	addr := &net.UnixAddr{
-		Net:  "unix",
-		Name: pipePath,
-	}
-	le.Debugf("connecting to winio socket: %s", addr.String())
-	return winio.DialPipeContext(ctx, pipePath)
+	pipeName := BuildPipeName(rootDir, pipeUuid)
+	le.Debugf("connecting to winio pipe: %s", pipeName)
+	return winio.DialPipeContext(ctx, pipeName)
+}
+
+// BuildPipeName builds a unique pipe name from a path and uuid.
+// uuid must be unique for rootDir
+func BuildPipeName(rootDir, pipeUuid string) string {
+	material := strings.Join([]string{rootDir, "uuid", pipeUuid}, "!")
+	var key [32]byte
+	blake3.DeriveKey("bldr pipesock windows Tue Apr 11 01:33:30 PM PDT 2023", []byte(material), key[:])
+	keyStr := b58.Encode(key[:])
+	return `\\.\pipe\bldr\` + keyStr
 }
