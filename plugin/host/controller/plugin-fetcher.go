81c5ccea07943701636a1b183ac6f5f4b6578cb4
Christian Stewart <christian@aperture.us>
2023-05-13 01:00:21 -0700

    fix(fetcher): more logging when fetching manifest
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- plugin/host/controller/plugin-fetcher.go
+++ plugin/host/controller/plugin-fetcher.go
@@ -172,6 +172,11 @@ func (t *pluginManifestFetcher) fetchManifest(ctx context.Context) (*bldr_manife
 			int(concurrentLimit),
 			nil,
 		)
+		if err == nil {
+			le.Infof("completed copying manifest to %s", pluginHostBucketID)
+		} else {
+			le.WithError(err).Warn("failed to copy manifest to %s", pluginHostBucketID)
+		}
 		return err
 	})
 	if err != nil {
