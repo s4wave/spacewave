49542e9a87446d61a275cb49d93d5cd2e31dfb3a
Christian Stewart <christian@aperture.us>
2023-12-28 19:52:40 -0800

    feat: add function to access unixfs via bus
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- unixfs/access/access.go
+++ unixfs/access/access.go
@@ -3,7 +3,9 @@ package unixfs_access
 import (
 	"context"
 
+	"github.com/aperturerobotics/controllerbus/bus"
 	"github.com/aperturerobotics/hydra/unixfs"
+	unixfs_errors "github.com/aperturerobotics/hydra/unixfs/errors"
 )
 
 // AccessUnixFSFunc is a function to access a UnixFS.
@@ -20,5 +22,41 @@ func NewAccessUnixFSFunc(handle *unixfs.FSHandle) AccessUnixFSFunc {
 		}
 		handle.AddReleaseCallback(released)
 		return handle, handle.Release, nil
+	}
+}
+
+// NewAccessUnixFSViaBusFunc builds a new func which accesses the UnixFS on the
+// given bus using the AccessUnixFS directive.
+//
+// If returnIfIdle is set: ErrFsNotFound is returned if not found.
+func NewAccessUnixFSViaBusFunc(b bus.Bus, unixfsID string, returnIfIdle bool) AccessUnixFSFunc {
+	return func(ctx context.Context, released func()) (*unixfs.FSHandle, func(), error) {
+		// access the directive via the bus
+		val, ref, err := ExAccessUnixFS(ctx, b, unixfsID, returnIfIdle, released)
+		if err == nil && val == nil {
+			err = unixfs_errors.ErrFsNotFound
+		}
+		if err != nil {
+			if ref != nil {
+				ref.Release()
+			}
+			return nil, nil, err
+		}
+
+		// call the inner access function
+		result, relResult, err := val(ctx, released)
+		rel := ref.Release
+		if relResult != nil {
+			rel = func() {
+				relResult()
+				ref.Release()
+			}
+		}
+		if err != nil {
+			rel()
+			return nil, nil, err
+		}
+
+		return result, rel, nil
 	}
 }
