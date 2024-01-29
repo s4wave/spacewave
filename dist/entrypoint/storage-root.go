44079c6c40ee7f6649d643626737b4a050578435
Christian Stewart <christian@aperture.us>
2024-01-28 19:47:13 -0800

    fix: use shorter config dir path on darwin
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- dist/entrypoint/storage-root.go
+++ dist/entrypoint/storage-root.go
@@ -3,15 +3,38 @@ package dist_entrypoint
 import (
 	"os"
 	"path/filepath"
+	"runtime"
+
+	"github.com/pkg/errors"
 )
 
 // DetermineStorageRoot determines the root dir to store data.
 func DetermineStorageRoot(projectID string) (string, error) {
-	configDir, err := os.UserConfigDir()
+	configDir, err := DetermineConfigDir()
 	if err != nil {
 		return "", err
 	}
 
-	outDir := filepath.Join(configDir, "aperture_robotics", projectID)
+	outDir := filepath.Join(configDir, projectID)
 	return outDir, nil
+}
+
+// DetermineConfigDir determines the root config dir.
+func DetermineConfigDir() (string, error) {
+	switch runtime.GOOS {
+	// Darwin has a limit on Unix socket names of 108 characters.
+	// Reduce the length of the path by using ~/.aperture
+	case "darwin":
+		dir := os.Getenv("HOME")
+		if dir == "" {
+			return "", errors.New("$HOME is not defined")
+		}
+		return filepath.Join(dir, ".aperture"), nil
+	default:
+		userConfDir, err := os.UserConfigDir()
+		if err != nil {
+			return "", err
+		}
+		return filepath.Join(userConfDir, "aperture"), nil
+	}
 }
