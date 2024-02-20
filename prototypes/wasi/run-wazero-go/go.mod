c87221dbe1c98b7b1313e2a109a0a73bbc054aa3
Christian Stewart <christian@aperture.us>
2024-02-19 16:43:31 -0800

    feat: add wazero prototype
    Signed-off-by: Christian Stewart <christian@aperture.us>

---
+++ prototypes/wasi/run-wazero-go/go.mod
@@ -0,0 +1,5 @@
+module run-wazero-go
+
+go 1.21
+
+require github.com/tetratelabs/wazero v1.6.0
---
+++ prototypes/wasi/run-wazero-go/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"embed"
+	"fmt"
+	"os"
+
+	"github.com/tetratelabs/wazero"
+	"github.com/tetratelabs/wazero/imports/wasi_snapshot_preview1"
+	"github.com/tetratelabs/wazero/sys"
+)
+
+// demoFS is an embedded filesystem
+//
+//go:embed go.mod go.sum
+var demoFS embed.FS
+
+func main() {
+	// Choose the context to use for function calls.
+	ctx := context.Background()
+
+	// Create a new WebAssembly Runtime.
+	r := wazero.NewRuntime(ctx)
+	defer r.Close(ctx) // This closes everything this Runtime created.
+
+	fsConf := wazero.NewFSConfig()
+	fsConf = fsConf.WithReadOnlyDirMount(".", "/")
+	fsConf = fsConf.WithDirMount("/tmp", "/tmp")
+	fsConf = fsConf.WithFSMount(demoFS, "/example")
+
+	config := wazero.NewModuleConfig().
+		WithStdout(os.Stdout).
+		WithStderr(os.Stderr).
+		WithFS(demoFS).
+		WithFSConfig(fsConf)
+
+	closeWasi, err := wasi_snapshot_preview1.Instantiate(ctx, r)
+	check(err)
+	defer func() {
+		_ = closeWasi.Close(ctx)
+	}()
+
+	demoWasm, err := os.ReadFile("../demo.wasm")
+	check(err)
+
+	args := config.WithArgs("demo.wasm")
+
+	// This is a convenience utility that chains CompileModule with
+	// InstantiateModule. To instantiate the same source multiple times, use
+	// CompileModule as InstantiateModule avoids redundant decoding and/or
+	// compilation.
+	if _, err = r.InstantiateWithConfig(ctx, demoWasm, args); err != nil {
+		// Note: Most compilers do not exit the module after running "_start",
+		// unless there was an error. This allows you to call exported functions.
+		if exitErr, ok := err.(*sys.ExitError); ok && exitErr.ExitCode() != 0 {
+			fmt.Fprintf(os.Stderr, "exit_code: %d\n", exitErr.ExitCode())
+		} else if !ok {
+			check(err)
+		}
+	}
+}
+
+func check(err error) {
+	if err != nil {
+		os.Stderr.WriteString(err.Error() + "\n")
+		os.Exit(1)
+	}
+}
