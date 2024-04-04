d5277fed1ffaa09bd611f6f42601de3d6625f8b2
Christian Stewart <christian@aperture.us>
2024-04-03 23:24:04 -0700

    chore: improve spacing
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- kvtx/kvfile/write.go
+++ kvtx/kvfile/write.go
@@ -45,39 +45,43 @@ func KvfileFromTx(ctx context.Context, writer io.Writer, tx kvtx.Tx, filterKeys 
 // Note: does not close the iterator.
 func KvfileFromIterator(ctx context.Context, writer io.Writer, it kvtx.Iterator, filterKeys FilterKeysFunc) error {
 	buf := make([]byte, 2*1024)
-	return kvfile.WriteIterator(writer, func() ([]byte, error) {
-		// iterate until we have a non-skipped key or the stream ends
-		for {
+	return kvfile.WriteIterator(
+		writer,
+		func() ([]byte, error) {
+			// iterate until we have a non-skipped key or the stream ends
+			for {
+				if err := ctx.Err(); err != nil {
+					return nil, context.Canceled
+				}
+				if !it.Next() {
+					return nil, it.Err()
+				}
+				key := it.Key()
+				if filterKeys != nil {
+					ok, err := filterKeys(key)
+					if err != nil {
+						return nil, err
+					}
+					if !ok {
+						continue
+					}
+				}
+				return key, nil
+			}
+		},
+		func(wr io.Writer, key []byte) (uint64, error) {
 			if err := ctx.Err(); err != nil {
-				return nil, context.Canceled
+				return 0, context.Canceled
 			}
-			if !it.Next() {
-				return nil, it.Err()
+			val, err := it.Value()
+			if err != nil {
+				return 0, err
 			}
-			key := it.Key()
-			if filterKeys != nil {
-				ok, err := filterKeys(key)
-				if err != nil {
-					return nil, err
-				}
-				if !ok {
-					continue
-				}
+			nw, err := io.CopyBuffer(wr, bytes.NewReader(val), buf)
+			if nw < 0 {
+				return 0, err
 			}
-			return key, nil
-		}
-	}, func(wr io.Writer, key []byte) (uint64, error) {
-		if err := ctx.Err(); err != nil {
-			return 0, context.Canceled
-		}
-		val, err := it.Value()
-		if err != nil {
-			return 0, err
-		}
-		nw, err := io.CopyBuffer(wr, bytes.NewReader(val), buf)
-		if nw < 0 {
-			return 0, err
-		}
-		return uint64(nw), err
-	})
+			return uint64(nw), err
+		},
+	)
 }
