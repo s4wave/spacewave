e12d31b1cd79b0985a5c2cb302972d07f8868d30
Christian Stewart <christian@aperture.us>
2024-06-02 21:49:58 -0700

    feat(errors): add error for empty object store key
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- object/errors.go
+++ object/errors.go
@@ -9,4 +9,6 @@ var (
 	ErrObjectStoreClosed = errors.New("object store is closed")
 	// ErrEmptyObjectStoreId is returned if the object store id was empty.
 	ErrEmptyObjectStoreId = errors.New("object store id is empty")
+	// ErrEmptyObjectStoreKey is returned if the object store key was empty.
+	ErrEmptyObjectStoreKey = errors.New("object store key is empty")
 )
