4946214e6e23c80866e7f47425f9301b9b9f0d33
Christian Stewart <christian@aperture.us>
2025-09-16 15:58:28 -0700

    fix: include sqlite in core
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- core/core_nojs.go
+++ core/core_nojs.go
@@ -9,6 +9,7 @@ import (
 	volume_badger "github.com/aperturerobotics/hydra/volume/badger"
 	volume_bolt "github.com/aperturerobotics/hydra/volume/bolt"
 	volume_redis "github.com/aperturerobotics/hydra/volume/redis"
+	volume_sqlite "github.com/aperturerobotics/hydra/volume/sqlite"
 )
 
 // addNativeFactories adds factories specific to this platform.
@@ -16,4 +17,5 @@ func addNativeFactories(b bus.Bus, sr *static.Resolver) {
 	sr.AddFactory(volume_badger.NewFactory(b))
 	sr.AddFactory(volume_bolt.NewFactory(b))
 	sr.AddFactory(volume_redis.NewFactory(b))
+	sr.AddFactory(volume_sqlite.NewFactory(b))
 }
