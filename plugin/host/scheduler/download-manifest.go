65b3efb30f5329cefe6309bbd3539124d59421a5
Christian Stewart <christian@aperture.us>
2026-02-21 03:59:10 -0800

    feat: implement manifest download using CopyBlockDAG
    Bump hydra dependency to get block/copy package. Implement
    execDownloadManifest to copy manifest block DAGs from source bucket
    to world bucket using CopyBlockDAG.
    
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- plugin/host/scheduler/download-manifest.go
+++ plugin/host/scheduler/download-manifest.go
@@ -4,15 +4,39 @@ import (
 	"context"
 
 	bldr_manifest "github.com/aperturerobotics/bldr/manifest"
+	block_copy "github.com/aperturerobotics/hydra/block/copy"
+	bucket_lookup "github.com/aperturerobotics/hydra/bucket/lookup"
+	"github.com/pkg/errors"
 )
 
-// execDownloadManifest executes downloading the manifest fetched from FetchManifest.
+// execDownloadManifest copies manifest blocks from the source bucket to the world bucket.
 func (t *pluginInstance) execDownloadManifest(ctx context.Context, manifestSnapshot *bldr_manifest.ManifestSnapshot) error {
 	if t.c.conf.GetDisableCopyManifest() {
 		return nil
 	}
 
-	// TODO
-	// t.le.Warnf("TODO execDownloadManifest: %v", manifestValue.String())
-	return nil
+	le := t.le
+	ref := manifestSnapshot.GetManifestRef()
+	blockRef := ref.GetRootRef()
+	if blockRef.GetEmpty() {
+		return errors.New("manifest ref has empty root block ref")
+	}
+
+	ws, err := t.c.worldStateCtr.WaitValue(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	// Access the world root bucket (dest) then the manifest source bucket (src).
+	return ws.AccessWorldState(ctx, nil, func(dest *bucket_lookup.Cursor) error {
+		return ws.AccessWorldState(ctx, ref, func(src *bucket_lookup.Cursor) error {
+			le.Infof("copying manifest DAG from bucket %s to %s", src.GetOpArgs().GetBucketId(), dest.GetOpArgs().GetBucketId())
+			err := block_copy.CopyBlockDAG(ctx, blockRef, bldr_manifest.NewManifestBlock, src.GetBucket(), dest.GetBucket())
+			if err != nil {
+				return errors.Wrap(err, "copy manifest block DAG")
+			}
+			le.Info("manifest download complete")
+			return nil
+		})
+	})
 }
