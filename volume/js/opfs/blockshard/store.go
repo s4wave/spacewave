40c19fcba4cde8755bffdca0ea235ae6f58da03e
Christian Stewart <christian@aperture.us>
2026-04-08 00:37:44 -0700

    feat(opfs): add block.StoreOps implementation for shard engine
    BlockStore wraps the shard engine to satisfy the block.StoreOps
    interface: PutBlock, GetBlock, GetBlockExists (bloom + segment scan),
    RmBlock (tombstone write), StatBlock, GetHashType. Compile-time type
    assertion included.
    
    Signed-off-by: Christian Stewart <christian@aperture.us>

---
+++ volume/js/opfs/blockshard/store.go
@@ -0,0 +1,145 @@
+//go:build js
+
+package blockshard
+
+import (
+	"context"
+
+	"github.com/aperturerobotics/bifrost/hash"
+	"github.com/aperturerobotics/hydra/block"
+	"github.com/aperturerobotics/hydra/volume/js/opfs/segment"
+	b58 "github.com/mr-tron/base58/base58"
+)
+
+// BlockStore wraps the shard engine to implement block.StoreOps.
+type BlockStore struct {
+	engine   *Engine
+	hashType hash.HashType
+}
+
+// NewBlockStore creates a block.StoreOps backed by the shard engine.
+func NewBlockStore(engine *Engine, hashType hash.HashType) *BlockStore {
+	return &BlockStore{engine: engine, hashType: hashType}
+}
+
+// GetHashType returns the preferred hash type.
+func (s *BlockStore) GetHashType() hash.HashType {
+	return s.hashType
+}
+
+// PutBlock puts a block into the store.
+func (s *BlockStore) PutBlock(ctx context.Context, data []byte, opts *block.PutOpts) (*block.BlockRef, bool, error) {
+	if len(data) == 0 {
+		return nil, false, block.ErrEmptyBlock
+	}
+
+	if opts == nil {
+		opts = &block.PutOpts{}
+	} else {
+		opts = opts.CloneVT()
+	}
+	opts.HashType = opts.SelectHashType(s.hashType)
+
+	ref, err := block.BuildBlockRef(data, opts)
+	if err != nil {
+		return nil, false, err
+	}
+	if forceRef := opts.GetForceBlockRef(); !forceRef.GetEmpty() {
+		if !ref.EqualsRef(forceRef) {
+			return ref, false, block.ErrBlockRefMismatch
+		}
+	}
+
+	key, err := encodeRef(ref)
+	if err != nil {
+		return nil, false, err
+	}
+
+	// Check if block already exists.
+	_, found, err := s.engine.Get([]byte(key))
+	if err != nil {
+		return nil, false, err
+	}
+	if found {
+		return ref, true, nil
+	}
+
+	// Write to shard engine.
+	entry := segment.Entry{
+		Key:   []byte(key),
+		Value: data,
+	}
+	if err := s.engine.Put(ctx, []segment.Entry{entry}); err != nil {
+		return nil, false, err
+	}
+	return ref, false, nil
+}
+
+// GetBlock gets a block by reference.
+func (s *BlockStore) GetBlock(ctx context.Context, ref *block.BlockRef) ([]byte, bool, error) {
+	if err := ref.Validate(false); err != nil {
+		return nil, false, err
+	}
+
+	key, err := encodeRef(ref)
+	if err != nil {
+		return nil, false, err
+	}
+
+	return s.engine.Get([]byte(key))
+}
+
+// GetBlockExists checks if a block exists.
+func (s *BlockStore) GetBlockExists(ctx context.Context, ref *block.BlockRef) (bool, error) {
+	key, err := encodeRef(ref)
+	if err != nil {
+		return false, err
+	}
+
+	_, found, err := s.engine.Get([]byte(key))
+	return found, err
+}
+
+// RmBlock deletes a block by writing a tombstone.
+func (s *BlockStore) RmBlock(ctx context.Context, ref *block.BlockRef) error {
+	key, err := encodeRef(ref)
+	if err != nil {
+		return err
+	}
+
+	entry := segment.Entry{
+		Key:       []byte(key),
+		Tombstone: true,
+	}
+	return s.engine.Put(ctx, []segment.Entry{entry})
+}
+
+// StatBlock returns metadata about a block without reading its data.
+func (s *BlockStore) StatBlock(ctx context.Context, ref *block.BlockRef) (*block.BlockStat, error) {
+	key, err := encodeRef(ref)
+	if err != nil {
+		return nil, err
+	}
+
+	val, found, err := s.engine.Get([]byte(key))
+	if err != nil {
+		return nil, err
+	}
+	if !found {
+		return nil, nil
+	}
+
+	return &block.BlockStat{Ref: ref, Size: int64(len(val))}, nil
+}
+
+// encodeRef encodes a BlockRef as a base58 string key.
+func encodeRef(ref *block.BlockRef) (string, error) {
+	rm, err := ref.MarshalKey()
+	if err != nil {
+		return "", err
+	}
+	return b58.FastBase58Encoding(rm), nil
+}
+
+// _ is a type assertion.
+var _ block.StoreOps = (*BlockStore)(nil)
