7937a7457db95aa07d645041af1345ddc89c860c
Christian Stewart <christian@aperture.us>
2026-05-04 23:38:55 -0700

    fix(manifest): access bucket-scoped manifest refs
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- bldr/manifest/world/access-manifest.go
+++ bldr/manifest/world/access-manifest.go
@@ -27,6 +27,36 @@ func AccessManifest(
 		assetsFS *unixfs.FSHandle,
 	) error,
 ) error {
+	if manifestRef != nil && manifestRef.GetBucketId() != "" {
+		return accessFunc(ctx, nil, func(root *bucket_lookup.Cursor) error {
+			if manifestRef.GetBucketId() == root.GetOpArgs().GetBucketId() {
+				le.WithField("bucket-id", manifestRef.GetBucketId()).Debug("following manifest ref in current bucket")
+				manifest, err := root.FollowRef(ctx, manifestRef)
+				if err != nil {
+					return err
+				}
+				defer manifest.Release()
+				le.WithField("bucket-id", manifestRef.GetBucketId()).Debug("accessing followed manifest in current bucket")
+				return bldr_manifest.AccessManifest(ctx, le, manifest, cb)
+			}
+
+			opArgs := root.GetOpArgs()
+			opArgs.BucketId = manifestRef.GetBucketId()
+			opArgs.VolumeId = ""
+			le.WithFields(logrus.Fields{
+				"bucket-id":      manifestRef.GetBucketId(),
+				"root-bucket-id": root.GetOpArgs().GetBucketId(),
+			}).Debug("following manifest ref in external bucket")
+			manifest, err := root.FollowRefWithOpArgs(ctx, manifestRef, opArgs)
+			if err != nil {
+				return err
+			}
+			defer manifest.Release()
+			le.WithField("bucket-id", manifestRef.GetBucketId()).Debug("accessing followed manifest in external bucket")
+			return bldr_manifest.AccessManifest(ctx, le, manifest, cb)
+		})
+	}
+
 	return accessFunc(ctx, manifestRef, func(bls *bucket_lookup.Cursor) error {
 		return bldr_manifest.AccessManifest(ctx, le, bls, cb)
 	})
