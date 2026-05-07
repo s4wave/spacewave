389e340a57451d382dfdf4f4dd12a047b0cc61d8
Christian Stewart <christian@aperture.us>
2026-05-06 18:32:49 -0700

    feat(plugin): import manifests into local host store
    Signed-off-by: Christian Stewart <christian@aperture.us>
    (cherry picked from commit d6f9830ce965bdf1889e4ea3d717273e05125b53)

--- cmd/spacewave/cli/plugin.go
+++ cmd/spacewave/cli/plugin.go
@@ -9,8 +9,15 @@ import (
 	"time"
 
 	"github.com/aperturerobotics/cli"
+	"github.com/aperturerobotics/controllerbus/config"
+	"github.com/aperturerobotics/controllerbus/controller"
 	"github.com/pkg/errors"
 	cli_entrypoint "github.com/s4wave/spacewave/bldr/cli/entrypoint"
+	bldr_manifest "github.com/s4wave/spacewave/bldr/manifest"
+	bldr_manifest_world "github.com/s4wave/spacewave/bldr/manifest/world"
+	"github.com/s4wave/spacewave/db/block"
+	block_transform "github.com/s4wave/spacewave/db/block/transform"
+	transform_s2 "github.com/s4wave/spacewave/db/block/transform/s2"
 	s4wave_space "github.com/s4wave/spacewave/sdk/space"
 )
 
@@ -26,8 +33,208 @@ func newPluginCommand(getBus func() cli_entrypoint.CliBus) *cli.Command {
 			buildPluginDenyCommand(),
 			buildPluginAddCommand(),
 			buildPluginRemoveCommand(),
+			buildPluginImportManifestCommand(getBus),
 		},
 	}
+}
+
+// buildPluginImportManifestCommand builds the plugin import-manifest subcommand.
+func buildPluginImportManifestCommand(getBus func() cli_entrypoint.CliBus) *cli.Command {
+	var dbPath string
+	var manifestID string
+	return &cli.Command{
+		Name:  "import-manifest",
+		Usage: "import a built manifest into the local plugin host store",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "db",
+				Usage:       "path to .bldr/ directory containing the devtool DB",
+				Required:    true,
+				Destination: &dbPath,
+			},
+			&cli.StringFlag{
+				Name:        "manifest-id",
+				Usage:       "manifest identifier",
+				Required:    true,
+				Destination: &manifestID,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			return runPluginImportManifest(c.Context, getBus, dbPath, manifestID)
+		},
+	}
+}
+
+func runPluginImportManifest(
+	ctx context.Context,
+	getBus func() cli_entrypoint.CliBus,
+	dbPath string,
+	manifestID string,
+) error {
+	cliBus := getBus()
+	if cliBus == nil {
+		return errors.New("bus not initialized")
+	}
+	le := cliBus.GetLogger()
+	src, err := openDevtoolVolume(ctx, le, dbPath)
+	if err != nil {
+		return errors.Wrap(err, "open devtool storage")
+	}
+	defer src.Close()
+	collected, err := lookupDevtoolManifest(ctx, le, src, manifestID)
+	if err != nil {
+		return errors.Wrap(err, "lookup manifest")
+	}
+	sfs := block_transform.NewStepFactorySet()
+	sfs.AddStepFactory(transform_s2.NewStepFactory())
+	transformConf, err := block_transform.NewConfig([]config.Config{&transform_s2.Config{}})
+	if err != nil {
+		return errors.Wrap(err, "build transform config")
+	}
+	xfrm, err := block_transform.NewTransformer(
+		controller.ConstructOpts{Logger: le},
+		sfs,
+		transformConf,
+	)
+	if err != nil {
+		return errors.Wrap(err, "build block transformer")
+	}
+	dest := cliBus.GetVolume()
+	rootRef := collected.ManifestRef.GetRootRef()
+	if err := copyManifestBlockDAG(ctx, rootRef, src, dest, xfrm); err != nil {
+		return errors.Wrap(err, "copy manifest blocks")
+	}
+	tx, err := cliBus.GetWorldEngine().NewTransaction(ctx, true)
+	if err != nil {
+		return errors.Wrap(err, "new transaction")
+	}
+	defer tx.Discard()
+	if _, err := bldr_manifest_world.CreateManifestStore(ctx, tx, daemonPluginHostObjectKey); err != nil {
+		return errors.Wrap(err, "create plugin host manifest store")
+	}
+	manifestKey := bldr_manifest.NewManifestKey(daemonPluginHostObjectKey, collected.Manifest.GetMeta())
+	objRef := collected.ManifestRef.Clone()
+	objRef.BucketId = "bldr/cli"
+	objRef.TransformConf = transformConf
+	if _, _, err := bldr_manifest_world.SetManifest(ctx, tx, "", manifestKey, objRef); err != nil {
+		return errors.Wrap(err, "set manifest")
+	}
+	if err := tx.SetGraphQuad(ctx, bldr_manifest_world.NewManifestQuad(daemonPluginHostObjectKey, manifestKey, manifestID)); err != nil {
+		return errors.Wrap(err, "link manifest")
+	}
+	if err := tx.Commit(ctx); err != nil {
+		return errors.Wrap(err, "commit manifest import")
+	}
+	os.Stdout.WriteString("imported: " + manifestKey + "\n")
+	return nil
+}
+
+func copyManifestBlockDAG(
+	ctx context.Context,
+	rootRef *block.BlockRef,
+	src block.StoreOps,
+	dest block.StoreOps,
+	xfrm block.Transformer,
+) error {
+	if rootRef.GetEmpty() {
+		return nil
+	}
+	visited := make(map[string]bool)
+	return copyManifestBlock(ctx, rootRef, bldr_manifest.NewManifestBlock, src, dest, xfrm, visited)
+}
+
+func copyManifestBlock(
+	ctx context.Context,
+	ref *block.BlockRef,
+	ctor block.Ctor,
+	src block.StoreOps,
+	dest block.StoreOps,
+	xfrm block.Transformer,
+	visited map[string]bool,
+) error {
+	if ref.GetEmpty() {
+		return nil
+	}
+	refStr := ref.MarshalString()
+	if visited[refStr] {
+		return nil
+	}
+	visited[refStr] = true
+	exists, err := dest.GetBlockExists(ctx, ref)
+	if err != nil {
+		return errors.Wrapf(err, "check block exists: %s", refStr)
+	}
+	var data []byte
+	if exists {
+		var found bool
+		data, found, err = dest.GetBlock(ctx, ref)
+		if err != nil {
+			return errors.Wrapf(err, "get existing block: %s", refStr)
+		}
+		if !found {
+			return errors.Wrapf(block.ErrNotFound, "existing block: %s", refStr)
+		}
+	} else {
+		var found bool
+		data, found, err = src.GetBlock(ctx, ref)
+		if err != nil {
+			return errors.Wrapf(err, "get block: %s", refStr)
+		}
+		if !found {
+			return errors.Wrapf(block.ErrNotFound, "block: %s", refStr)
+		}
+		if _, _, err := dest.PutBlock(ctx, data, nil); err != nil {
+			return errors.Wrapf(err, "put block: %s", refStr)
+		}
+	}
+	if ctor == nil {
+		return nil
+	}
+	decoded := data
+	if xfrm != nil {
+		decoded, err = xfrm.DecodeBlock(data)
+		if err != nil {
+			return errors.Wrapf(err, "decode block: %s", refStr)
+		}
+	}
+	blk := ctor()
+	if err := blk.UnmarshalBlock(decoded); err != nil {
+		return errors.Wrapf(err, "unmarshal block: %s", refStr)
+	}
+	return followManifestBlockRefs(ctx, blk, src, dest, xfrm, visited)
+}
+
+func followManifestBlockRefs(
+	ctx context.Context,
+	blk any,
+	src block.StoreOps,
+	dest block.StoreOps,
+	xfrm block.Transformer,
+	visited map[string]bool,
+) error {
+	if withRefs, ok := blk.(block.BlockWithRefs); ok {
+		refs, err := withRefs.GetBlockRefs()
+		if err != nil {
+			return errors.Wrap(err, "get block refs")
+		}
+		for id, childRef := range refs {
+			childCtor := withRefs.GetBlockRefCtor(id)
+			if err := copyManifestBlock(ctx, childRef, childCtor, src, dest, xfrm, visited); err != nil {
+				return err
+			}
+		}
+	}
+	if withSubBlocks, ok := blk.(block.BlockWithSubBlocks); ok {
+		for _, sub := range withSubBlocks.GetSubBlocks() {
+			if sub == nil || sub.IsNil() {
+				continue
+			}
+			if err := followManifestBlockRefs(ctx, sub, src, dest, xfrm, visited); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
 
 // buildPluginListCommand builds the plugin list subcommand.
