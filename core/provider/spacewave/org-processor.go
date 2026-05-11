5469e95756a5fd241aa8920c196309fb5d656045
Christian Stewart <christian@aperture.us>
2026-05-11 13:31:23 -0700

    fix(org): resync processors on org cache updates
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- core/provider/spacewave/org-processor.go
+++ core/provider/spacewave/org-processor.go
@@ -44,32 +44,118 @@ func (a *ProviderAccount) watchOrgProcessors(ctx context.Context) error {
 	processors.SetContext(ctx, true)
 	defer processors.SetContext(nil, false)
 
+	watchCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
 	soList, err := a.soListCtr.WaitValue(ctx, nil)
 	if err != nil {
 		return err
 	}
 
+	soListChanged := make(chan *sobject.SharedObjectList, 1)
+	orgListChanged := make(chan struct{}, 1)
+	errCh := make(chan error, 2)
+
+	go a.watchOrgProcessorSOList(watchCtx, soList, soListChanged, errCh)
+	go a.watchOrgProcessorOrgList(watchCtx, orgListChanged)
+
 	var prevOrgIDs []string
 	for {
-		var orgIDs []string
-		for _, entry := range soList.GetSharedObjects() {
-			if entry.GetMeta().GetBodyType() != s4wave_org.OrgBodyType {
-				continue
-			}
-			orgID := entry.GetRef().GetProviderResourceRef().GetId()
-			if !a.HasCachedOwnerOrganization(orgID) {
-				continue
-			}
-			orgIDs = append(orgIDs, orgID)
-		}
+		orgIDs := a.orgProcessorKeys(soList)
 		if !slices.Equal(orgIDs, prevOrgIDs) {
 			processors.SyncKeys(orgIDs, false)
 			prevOrgIDs = orgIDs
 		}
 
-		soList, err = a.soListCtr.WaitValueChange(ctx, soList, nil)
-		if err != nil {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case err := <-errCh:
 			return err
+		case next := <-soListChanged:
+			soList = next
+		case <-orgListChanged:
 		}
 	}
+}
+
+func (a *ProviderAccount) watchOrgProcessorSOList(
+	ctx context.Context,
+	current *sobject.SharedObjectList,
+	changed chan<- *sobject.SharedObjectList,
+	errCh chan<- error,
+) {
+	for {
+		next, err := a.soListCtr.WaitValueChange(ctx, current, nil)
+		if err != nil {
+			select {
+			case errCh <- err:
+			case <-ctx.Done():
+			}
+			return
+		}
+		current = next
+		select {
+		case changed <- next:
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func (a *ProviderAccount) watchOrgProcessorOrgList(
+	ctx context.Context,
+	changed chan<- struct{},
+) {
+	for {
+		var ch <-chan struct{}
+		a.orgBcast.HoldLock(func(_ func(), getWaitCh func() <-chan struct{}) {
+			ch = getWaitCh()
+		})
+		select {
+		case <-ctx.Done():
+			return
+		case <-ch:
+		}
+		select {
+		case changed <- struct{}{}:
+		default:
+		}
+	}
+}
+
+func (a *ProviderAccount) orgProcessorKeys(
+	soList *sobject.SharedObjectList,
+) []string {
+	if soList == nil {
+		return nil
+	}
+
+	ownerOrgs := make(map[string]struct{})
+	a.orgBcast.HoldLock(func(_ func(), _ func() <-chan struct{}) {
+		if !a.orgListValid {
+			return
+		}
+		for _, org := range a.orgList {
+			if isOrganizationOwnerRole(org.GetRole()) {
+				ownerOrgs[org.GetId()] = struct{}{}
+			}
+		}
+	})
+	if len(ownerOrgs) == 0 {
+		return nil
+	}
+
+	var orgIDs []string
+	for _, entry := range soList.GetSharedObjects() {
+		if entry.GetMeta().GetBodyType() != s4wave_org.OrgBodyType {
+			continue
+		}
+		orgID := entry.GetRef().GetProviderResourceRef().GetId()
+		if _, ok := ownerOrgs[orgID]; !ok {
+			continue
+		}
+		orgIDs = append(orgIDs, orgID)
+	}
+	return orgIDs
 }
