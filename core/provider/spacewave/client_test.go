7b7d6c3ac727d6a13ece6459f4a9efda80e44aeb
Christian Stewart <christian@aperture.us>
2026-05-24 09:53:10 -0700

    test(goscript): expose provider test helper
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- core/provider/spacewave/client_test.go
+++ core/provider/spacewave/client_test.go
@@ -5,7 +5,6 @@ package provider_spacewave
 import (
 	"bytes"
 	"context"
-	"crypto/rand"
 	"crypto/sha256"
 	"encoding/base64"
 	"encoding/hex"
@@ -22,20 +21,6 @@ import (
 	"github.com/s4wave/spacewave/net/crypto"
 	"github.com/s4wave/spacewave/net/peer"
 )
-
-// generateTestKeypair generates an Ed25519 keypair for testing.
-func generateTestKeypair(t *testing.T) (crypto.PrivKey, peer.ID) {
-	t.Helper()
-	priv, _, err := crypto.GenerateEd25519Key(rand.Reader)
-	if err != nil {
-		t.Fatalf("generating key: %v", err)
-	}
-	pid, err := peer.IDFromPrivateKey(priv)
-	if err != nil {
-		t.Fatalf("deriving peer ID: %v", err)
-	}
-	return priv, pid
-}
 
 // newTestSignedClient builds a SignedHTTPClient pointed at a test server.
 func newTestSignedClient(t *testing.T, baseURL string) (*SignedHTTPClient, peer.ID) {
