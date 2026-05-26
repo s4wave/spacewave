7d14e941c2bca60d6d7827b2dbdd726aca5f90de
Christian Stewart <christian@aperture.us>
2026-05-26 08:58:02 -0700

    test(goscript): enable account fetcher tests
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- core/provider/spacewave/sobject_test.go
+++ core/provider/spacewave/sobject_test.go
@@ -816,23 +816,6 @@ func TestFetchSharedObjectListPreservesCreatedCacheEntry(t *testing.T) {
 	}
 }
 
-func mustMarshalVT(t *testing.T, marshaler interface{ MarshalVT() ([]byte, error) }) []byte {
-	t.Helper()
-	data, err := marshaler.MarshalVT()
-	if err != nil {
-		t.Fatalf("marshal protobuf: %v", err)
-	}
-	return data
-}
-
-func mustMarshalSOStateMessageSnapshotJSON(t *testing.T, state *sobject.SOState) []byte {
-	t.Helper()
-	return mustMarshalVT(t, &api.SOStateMessage{
-		Seqno:   1,
-		Content: &api.SOStateMessage_Snapshot{Snapshot: state},
-	})
-}
-
 func TestEnsureSharedObjectListLoaded_NoSubscriptionSkipsFetch(t *testing.T) {
 	var listCalls int
 
