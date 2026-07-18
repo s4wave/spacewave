0a183314b87554aa1c3b9ae529a76bcd42939d4f
Christian Stewart <christian@aperture.us>
2026-07-18 16:34:55 -0700

    fix(forge): demote unhandled job tracker operations
    Signed-off-by: Christian Stewart <christian@aperture.us>

---
+++ forge/cluster/controller/job-tracker_test.go
@@ -0,0 +1,32 @@
+package cluster_controller
+
+import (
+	"context"
+	"testing"
+
+	"github.com/s4wave/spacewave/db/bucket"
+	"github.com/s4wave/spacewave/db/world"
+	"github.com/sirupsen/logrus"
+)
+
+func TestSkipUnhandledOperation(t *testing.T) {
+	handler := skipUnhandledOperation(func(
+		context.Context,
+		*logrus.Entry,
+		world.WorldState,
+		world.ObjectState,
+		*bucket.ObjectRef,
+		uint64,
+	) (bool, error) {
+		return false, world.ErrUnhandledOp
+	})
+
+	waitForChanges, err := handler(context.Background(), nil, nil, nil, nil, 0)
+	if err != nil {
+		t.Fatalf("handler error = %v, want nil", err)
+	}
+	if !waitForChanges {
+		t.Fatal("waitForChanges = false, want true")
+	}
+
+}
