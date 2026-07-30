c077cdf71aa8a3da26e517af415fd10e0dbfd9ff
Christian Stewart <christian@aperture.us>
2026-07-29 23:58:55 -0700

    test(bldr): prove post-execute tracker joins
    Both shape-3 controllers now have owner tests that create tracker work after
    Execute has returned. Each test holds the routine after cancellation and
    requires Close to remain blocked until the stop signal, proving a joined
    shutdown rather than cancellation-only behavior.
    
    Signed-off-by: Christian Stewart <christian@aperture.us>

---
+++ bldr/project/controller/close_test.go
@@ -0,0 +1,62 @@
+//go:build !js
+
+package bldr_project_controller
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/aperturerobotics/util/keyed"
+	"github.com/sirupsen/logrus"
+)
+
+func TestCloseJoinsPostExecuteTrackerWork(t *testing.T) {
+	ctrl := NewController(logrus.NewEntry(logrus.New()), nil, &Config{})
+	if err := ctrl.Execute(context.Background()); err != nil {
+		t.Fatal(err)
+	}
+
+	started := make(chan struct{})
+	release := make(chan struct{})
+	stopped := make(chan struct{})
+	ctrl.manifestBuilders = keyed.NewKeyedRefCount(func(string) (keyed.Routine, *manifestBuilderTracker) {
+		return ctrl.routines.wrap(func(ctx context.Context) error {
+			close(started)
+			<-ctx.Done()
+			<-release
+			close(stopped)
+			return nil
+		}), &manifestBuilderTracker{}
+	})
+	ctrl.manifestBuilders.SetContext(context.Background(), true)
+	ctrl.manifestBuilders.AddKeyRef("post-execute")
+	select {
+	case <-started:
+	case <-time.After(5 * time.Second):
+		t.Fatal("manifest tracker did not start")
+	}
+
+	closeDone := make(chan struct{})
+	go func() {
+		_ = ctrl.Close()
+		close(closeDone)
+	}()
+	select {
+	case <-closeDone:
+		t.Fatal("Close returned before the tracker stopped")
+	case <-time.After(100 * time.Millisecond):
+	}
+
+	close(release)
+	select {
+	case <-closeDone:
+	case <-time.After(5 * time.Second):
+		t.Fatal("Close did not return after the tracker stopped")
+	}
+	select {
+	case <-stopped:
+	default:
+		t.Fatal("Close returned before the tracker stop signal")
+	}
+}
